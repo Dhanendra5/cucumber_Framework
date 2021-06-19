8489ac8d9a7344e356da9ea738c3631b5dbe1029
pc <pc@mypc>
2021-06-19 14:04:27 +0530

    first commit

---
+++ src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,55 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Steps {
+
+	
+	@Given("^user launch chromebrowser$")
+	public void user_launch_chromebrowser()  {
+		
+		
+	   
+	    
+	}
+
+	@When("^user opens url \"([^\"]*)\"$")
+	public void user_opens_url(String arg1)  {
+	   
+	    
+	}
+
+	@When("^user enter email as \"([^\"]*)\" and passsword as \"([^\"]*)\"$")
+	public void user_enter_email_as_and_passsword_as(String arg1, String arg2)  {
+	   
+	    
+	}
+
+	@When("^click on login$")
+	public void click_on_login()  {
+	   
+	    
+	}
+
+	@Then("^page title should be \"([^\"]*)\"$")
+	public void page_title_should_be(String arg1)  {
+	   
+	    
+	}
+
+	@When("^user clicks on logout link$")
+	public void user_clicks_on_logout_link()  {
+	   
+	    
+	}
+
+	@Then("^close browser$")
+	public void close_browser()  {
+	   
+	    
+	}
+
+
+}
